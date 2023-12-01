62c7fd5032f438602bf099629b01326681bd51ea
trixtao <139524366+trixtao@users.noreply.github.com>
2023-12-01 16:24:57 +0800

    Update most_wins.sql

--- most_wins.sql
+++ most_wins.sql
@@ -1,1 +1,23 @@
+/* This query shows the number of wins a FIlipino has (for a specific year) along with the events they won */
 
+SELECT 
+    r.personId,
+    COUNT(*) AS Num1stPlaces,
+    COUNT(DISTINCT(r.eventId)) as 1stPlaceEvents,
+    GROUP_CONCAT(DISTINCT(r.eventId) SEPARATOR ', ') as EventList
+FROM 
+    Results r 
+JOIN 
+    Competitions c 
+ON 
+    c.id = r.competitionId 
+WHERE 
+    r.best > 0 AND 
+    r.pos = 1 AND 
+    r.roundTypeId IN ('c', 'f') AND 
+    r.CountryId = "Philippines" AND
+    c.end_date LIKE ':year%'
+GROUP BY
+    r.personId
+ORDER BY 
+    Num1stPlaces DESC 
