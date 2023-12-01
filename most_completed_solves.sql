86367ad0273f62ec9289647ce206070efeb0580d
trixtao <139524366+trixtao@users.noreply.github.com>
2023-12-01 16:47:00 +0800

    Update most_completed_solves.sql

--- most_completed_solves.sql
+++ most_completed_solves.sql
@@ -1,1 +1,26 @@
+/* This query shows the number of completed solves a Filipino has done (for a specific year) */
+
+SELECT 
+    num.personId, 
+    num.name,
+    num.solves
+FROM (
+    SELECT 
+        r.personId, 
+        r.personName AS name,
+        COUNT(DISTINCT r.competitionId) as numComps,
+        SUM(
+            CASE WHEN r.value1 > 0 THEN 1 ELSE 0 END +
+            CASE WHEN r.value2 > 0 THEN 1 ELSE 0 END +
+            CASE WHEN r.value3 > 0 THEN 1 ELSE 0 END +
+            CASE WHEN r.value4 > 0 THEN 1 ELSE 0 END +
+            CASE WHEN r.value5 > 0 THEN 1 ELSE 0 END
+        ) as solves
+    FROM Results r
+    JOIN Competitions c ON r.competitionId = c.id
+    WHERE r.countryId = 'Philippines'
+      AND c.end_date LIKE ':year%'
+    GROUP BY r.personId
+) num
+ORDER BY num.solves DESC;
 
