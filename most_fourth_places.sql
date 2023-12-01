c3d03c39e54ce0f697c7b5dc37ff9da2137741fc
trixtao <139524366+trixtao@users.noreply.github.com>
2023-12-01 16:17:07 +0800

    Update most_fourth_places.sql

--- most_fourth_places.sql
+++ most_fourth_places.sql
@@ -1,1 +1,22 @@
+SELECT 
+    r.personId,
+    COUNT(*) AS Num4thPlaces,
+    COUNT(DISTINCT(r.eventId)) as 4thPlaceEvents,
+    GROUP_CONCAT(DISTINCT(r.eventId) SEPARATOR ', ') as EventList
+FROM 
+    Results r 
+JOIN 
+    Competitions c 
+ON 
+    c.id = r.competitionId 
+WHERE 
+    r.best > 0 AND 
+    r.pos = 4 AND 
+    r.roundTypeId IN ('c', 'f') AND 
+    r.CountryId = "Philippines" AND
+    c.end_date LIKE ':year%'
+GROUP BY
+    r.personId
+ORDER BY 
+    Num4thPlaces DESC 
 
